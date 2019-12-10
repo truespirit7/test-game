c4672742256896ad7974e8fc06742614327e2af7
AndreevDA <AndreevDA@DOM201.LOCAL>
2019-12-10 16:26:54 +0700

    'test'

---
+++ v1-test-game/Assets/Scripts/PlayerAnimator.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimator : MonoBehaviour
+{
+    // Start is called before the first frame update
+    SpriteRenderer[] sprites;
+    public float framesPerSecond;
+    private SpriteRenderer spriteRenderer;
+    void Start()
+    {
+        spriteRenderer = GetComponent<Renderer>() as SpriteRenderer; 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int index = (int)(Time.timeSinceLevelLoad * framesPerSecond);
+        index = index % sprites.Length;
+        spriteRenderer.sprite = sprites[index]; 
+        
+    }
+}
