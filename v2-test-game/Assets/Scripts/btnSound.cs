9e821c5b975b815fdd349e2e55fe2ac95c4aa21c
truespirit7 <truespirit7777@gmail.com>
2019-12-22 18:45:33 +0700

    + sound

---
+++ v2-test-game/Assets/Scripts/btnSound.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class btnSound : MonoBehaviour
+{
+    public AudioSource mySound;
+    public AudioClip ClickFX;
+
+    public void ClickSound()
+    {
+        mySound.PlayOneShot(ClickFX);
+    }
+
+}
